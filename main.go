32e7a1963a75ff51d6fe0d039a0d44c73fc9769c
vasiliy249 <v.ivanischev@gmail.com>
2018-01-28 22:36:47 +0300

    some imprv

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"strconv"
 	"os"
 	"bufio"
+	"io"
+	"encoding/binary"
 )
 
 var (
@@ -31,6 +33,29 @@ func handleConn(conn net.Conn) {
 	}
 	if reply == "PING\n" {
 		conn.Write([]byte("PONG\n"))
+	} else if reply == "FILE\n" {
+		fileSizeSlice := make([]byte, 8)
+		conn.Read(fileSizeSlice)
+		fileSize := binary.BigEndian.Uint64(fileSizeSlice)
+		file, err := os.Create("X:/test_rcv.jpg")
+		if err != nil {
+			fmt.Println("Error during file creating")
+			return
+		}
+		defer file.Close()
+		written, err := io.CopyN(file, conn, int64(fileSize))
+		if err != nil {
+			fmt.Println("Error during file receiving")
+			return
+		}
+		allBytes := written == int64(fileSize)
+		if !allBytes {
+			fmt.Println("Not all bytes were received", written, fileSize)
+			conn.Write([]byte("Not all bytes were received \n"))
+		} else {
+			fmt.Println("The file was received")
+			conn.Write([]byte("The file was received \n"))
+		}
 	}
 }
 
@@ -68,6 +93,58 @@ func sendPing(addr string) {
 	fmt.Print("Message from server: " + reply)
 }
 
+func sendFile(addr string) {
+	//get filename
+	fmt.Print("Enter filename: ")
+	var strFilename string
+	fmt.Scanln(&strFilename)
+
+	// establishing a connection
+	conn, err := net.Dial(CONN_TYPE, addr + ":" + strconv.Itoa(port))
+	if err != nil {
+		fmt.Println("Error during establish connection with ", addr)
+		return
+	}
+	defer conn.Close()
+
+	// file openning
+	fileReader, err := os.Open(strFilename)
+	if err != nil {
+		fmt.Println("Error during file openning")
+		return
+	}
+	defer fileReader.Close()
+	fi, err := fileReader.Stat()
+	if err != nil {
+		fmt.Println("Error getting file info")
+		return
+	}
+	fileSize := fi.Size()
+	fmt.Println("File size: ", fileSize)
+
+	// send to server
+	conn.Write([]byte("FILE\n"))
+	fileSizeSlice := make([]byte, 8)
+	binary.BigEndian.PutUint64(fileSizeSlice, uint64(fileSize))
+	conn.Write(fileSizeSlice)
+	written, err := io.Copy(conn, fileReader)
+	if err != nil {
+		fmt.Println("Error sending file")
+		return
+	}
+	if written != fileSize {
+		fmt.Println("Not all bytes were sent")
+	}
+
+	// check reply
+	reply, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error during read from ", addr)
+		return
+	}
+	fmt.Print("File was sent to the server. Reply: " + reply)
+}
+
 func main() {
 	processCmdArgs()
 	fmt.Println("Launching server on port", port, "...")
@@ -80,7 +157,16 @@ func main() {
 			fmt.Println("Wrong IP address")
 			continue
 		} else {
-			sendPing(strIP)
+			fmt.Print("Enter command (file/ping):")
+			var cmd string
+			fmt.Scanln(&cmd)
+			if (cmd == "ping") {
+				sendPing(strIP)
+			} else if cmd == "file" {
+				sendFile(strIP)
+			} else {
+				fmt.Print("wrong command.")
+			}
 		}
 	}
 }
